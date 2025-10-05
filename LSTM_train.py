572456e7f58321dd210e96642bce3a5f8a1d4a4f
matejvu <vm.vuckovic@outlook.com>
2025-10-05 22:32:02 +0200

    Improve training function and include train/val loss plot

--- LSTM_train.py
+++ LSTM_train.py
@@ -5,30 +5,49 @@ Created on Sun Oct  5 17:34:24 2025
 @author: matej
 """
 
-#Hyperparameters
-import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import ExponentialLR
+import numpy as np
+import matplotlib.pyplot as plt
+
 from LSTM_model import LSTMnoTF
 
-path = "./datasets_npz_awng/lorenz_dataset_50dB.npz"
-data = np.load(path)
-dt = data["dt"]
-init = data["init"]
-D = data["dimension"]
+def plot_training_curves(train_losses, val_losses, title = 'Training and Validation Loss'):
+    plt.figure(figsize=(10, 6))
+    
+    epochs = range(1, len(train_losses) + 1)
+    
+    plt.plot(epochs, train_losses, 'b-', label='Training Loss', linewidth=2)
+    plt.plot(epochs, val_losses, 'r-', label='Validation Loss', linewidth=2)
+    
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    # plt.yscale('log')  # Useful if loss values vary widely
+    plt.tight_layout()
+    plt.show()
 
 
-lags = {2, 4, 6}
-batch = {16, 32, 64}
-num_layers = {1,2,3}
 
 
 
 #=========================================
+#   ARGS
 #       path - path to npz file
 #       d - number of lags
 #       t - number of predicted steps
+#   RETURN
+#       loaders - tuple of DataLoaders
+#       datasets - tuple of Datasets
+#       D - dimension of data
+
 #=========================================
-def TDatasetFromSeries(path, d, t,particles = 1, data_len = 1000):
+def TDatasetFromSeries(path, d, t, batch_size, particles = 1, data_len = 1000):
     data = np.load(path)
     dt = data["dt"]
     init = data["init"]
@@ -37,59 +56,67 @@ def TDatasetFromSeries(path, d, t,particles = 1, data_len = 1000):
     X = data['X'][0][0:data_len]
     Y = data['Y'][0][0:data_len]
     Z = data['Z'][0][0:data_len]
+    
     timeseries = np.stack([X, Y, Z], axis=-1)
-    print(X[3], Y[3], Z[3])
-    print(timeseries[3])
+    
     if(particles!=1):
         print('Multiple particles not yet supported!')
         return
     
-    train_d = []
-    train_out = []
+    data_in = []
+    data_out = []
 
     for i in range(len(X) - d - t):
-        window = X[i:i+d]
-        output = X[i+d:i+d+t]
+        window = timeseries[i:i+d]
+        output = timeseries[i+d:i+d+t]
         
-        train_d.append(window)
-        train_out.append(output)
+        data_in.append(window)
+        data_out.append(output)
 
 
-    train_d = np.array(train_d)    
-    train_out = np.array(train_out)    
+    data_in = np.array(data_in)    
+    data_out = np.array(data_out)    
     
-    train_d = np.expand_dims(train_d, axis=-1)
-    train_out = np.expand_dims(train_out, axis=-1)
+    # train_d = np.expand_dims(train_d, axis=-1)
     # train_out = np.expand_dims(train_out, axis=-1)
-    # print(train_d.shape)
     
-    # Convert to PyTorch tensors FIRST
-    train_d_tensor = torch.FloatTensor(train_d)
-    train_out_tensor = torch.FloatTensor(train_out)
+    # Convert to PyTorch tensors 
+    data_in = torch.FloatTensor(data_in)
+    data_out = torch.FloatTensor(data_out)
     
-    # THEN split and create datasets
-    train_size = int(0.8 * len(train_d_tensor))
+    # Split data to Train Val Test - 70/15/15
+    train_size = int(0.7 * len(data_in))
+    val_size = int(0.85 * len(data_in))
     
-    train_dataset = TensorDataset(train_d_tensor[:train_size], train_out_tensor[:train_size])
-    val_dataset = TensorDataset(train_d_tensor[train_size:], train_out_tensor[train_size:])
+    #Datasets
+    train_dataset = TensorDataset(data_in[:train_size], data_out[:train_size])
+    val_dataset = TensorDataset(data_in[train_size:val_size], data_out[train_size:val_size])
+    test_dataset = TensorDataset(data_in[val_size:], data_out[val_size:])
     
-    
-    # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    #DataLoaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    
+    return (train_loader, val_loader, test_loader), (train_dataset, val_dataset, test_dataset), D
     
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Training function
-def train_lstm_model(model, train_loader, val_loader, epochs=100, learning_rate=0.001):
+#=========================================
+#   ARGS
+#       model - LSTM model
+#       train_loader - 
+#       val_loader - 
+#       epochs - 
+#       learning_rate - 
+#   RETURN
+#       train_losses
+#       val_losses
+#=========================================
+def train_lstm_model(model, train_loader, val_loader, epochs=100, learning_rate=0.001, gamma = 0.95):
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = ExponentialLR(optimizer, gamma=gamma)
     
     train_losses = []
     val_losses = []
@@ -99,14 +126,13 @@ def train_lstm_model(model, train_loader, val_loader, epochs=100, learning_rate=
         model.train()
         train_loss = 0.0
         
+        # Train Batches
         for batch_idx, (data, target) in enumerate(train_loader):
             optimizer.zero_grad()
-            
+        
             # Forward pass
             predictions = model(data, future_steps=target.shape[1])
             
-            # print(predictions.shape)
-            # print(f'Target:{target.shape}')
             # Calculate loss
             loss = criterion(predictions, target)
             
@@ -115,6 +141,9 @@ def train_lstm_model(model, train_loader, val_loader, epochs=100, learning_rate=
             optimizer.step()
             
             train_loss += loss.item()
+        
+        scheduler.step()
+        #current_lr = scheduler.get_last_lr()[0]
         
         # Validation phase
         model.eval()
@@ -138,23 +167,14 @@ def train_lstm_model(model, train_loader, val_loader, epochs=100, learning_rate=
     
     return train_losses, val_losses
 
-# Main training execution
-if __name__ == "__main__":
+
+def train(path, d, t, batch_size, hidden_size, epochs, lr, criterion, verbose = False):
     
-    input_size = 1
-    output_size = 1
-    max_future_steps = 10
-    hidden_size = 12  # You can test different sizes
-    batch_size = 4
-    epochs = 10
-    
-    #make time frames
-    
-    
-    # print(f"Training samples: {len(train_dataset)}")
-    # print(f"Validation samples: {len(val_dataset)}")
+    #load data
+    (TrainLD, ValLD, TestLD),(TrainDS, ValDS, TestDS), D = TDatasetFromSeries(path, d, t, batch_size)
     
     # Initialize model
+    input_size = output_size = D    
     model = LSTMnoTF(input_size, hidden_size, output_size, max_future_steps)
     
     # Print model info
@@ -162,31 +182,57 @@ if __name__ == "__main__":
     print(f"\nModel initialized:")
     print(f"Hidden size: {hidden_size}")
     print(f"Total parameters: {total_params}")
-    print(f"Input shape: {train_d.shape}")
-    print(f"Target shape: {train_out.shape}")
+    print(f"Input DS shape: {TrainDS.tensors[0].shape}")
+    print(f"Output DS shape: {TrainDS.tensors[1].shape}")
     
     # Train the model
     print("\nStarting training...")
     train_losses, val_losses = train_lstm_model(
-        model, train_loader, val_loader, epochs=epochs, learning_rate=0.001
+        model, TrainLD, ValLD, epochs=epochs, learning_rate=lr
     )
+    
+    
+    #PLot
+    if verbose:
+        t = 'Traning['++']'
+        plot_training_curves(train_losses, val_losses)
     
     # Test the trained model
     print("\nTesting trained model...")
-    model.eval()
-    
-    print(train_dataset[:5][0].shape)
+    test_loss = 0.0
     
     with torch.no_grad():
-        test_sample = train_dataset[:5][0]  # Test on first 5 samples
-        predictions = model(test_sample, future_steps=max_future_steps)
-        
-        print(f"Sample input shape: {test_sample.shape}")
-        print(f"Predictions shape: {predictions.shape}")
-        print(f"Target shape: {train_out[:5].shape}")
-        
-        # Calculate test loss
-        target_tensor = torch.FloatTensor(train_out[:5])
-        test_loss = nn.functional.mse_loss(predictions, target_tensor)
-        print(f"Test loss on sample: {test_loss:.6f}")
+        for data, target in TestLD:
+            predictions = model(data, future_steps=target.shape[1])
+            loss = nn.functional.mse_loss(predictions, target)
+            test_loss += loss.item()
+    
+    avg_test_loss = test_loss / len(TestLD)
+    print(f'Avg. test loss: {avg_test_loss}')
+    
 
+
+
+path = "./datasets_npz/lorenz_dataset.npz"
+data = np.load(path)
+dt = data["dt"]
+init = data["init"]
+D = data["dimension"]
+
+
+lags = {2, 4, 6}
+batch = {16, 32, 64}
+num_layers = {1,2,3}
+
+d = 4
+t = 10
+
+lr = 0.001
+input_size = 1
+output_size = 1
+max_future_steps = 10
+hidden_size = 12  # You can test different sizes
+batch_size = 4
+epochs = 20
+
+train(path, d, t, batch_size, hidden_size, epochs,lr,  None, True)
